9cf0891c2d60ad883d49c145c196565c52f2b98b
Argon <techyargonn@gmail.com>
2024-06-26 11:04:15 +0530

    debugging code

--- illustrations/views.py
+++ illustrations/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'illustrations/index.html')
+
+def artists(request):
+    return render(request, 'illustrations/artists.html')
+
+def contact(request):
+    return render(request, 'illustrations/contact.html')
